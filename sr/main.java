2d38b4fc72447e959218b0753ec756eb5c1b3b33
naveenhiranya2 <naveenhiranya2005@gmail.com>
2026-06-14 11:19:39 +0530

    Update main.java

--- sr/main.java
+++ sr/main.java
@@ -43,3 +43,32 @@ public class Main {
         scanner.close();
     }
 }
+public class Player {
+
+    String name;
+    int hp;
+    int attack;
+    int defense;
+    int gold;
+    int level;
+
+    public Player(String name) {
+        this.name = name;
+        this.hp = 100;
+        this.attack = 15;
+        this.defense = 5;
+        this.gold = 0;
+        this.level = 1;
+    }
+
+    public void showStats() {
+        System.out.println("\n===== PLAYER STATS =====");
+        System.out.println("Name    : " + name);
+        System.out.println("HP      : " + hp);
+        System.out.println("Attack  : " + attack);
+        System.out.println("Defense : " + defense);
+        System.out.println("Gold    : " + gold);
+        System.out.println("Level   : " + level);
+        System.out.println("========================");
+    }
+}
