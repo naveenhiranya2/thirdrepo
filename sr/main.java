aff9e4414812f7f92facd5d880f86f46cecd5155
naveenhiranya2 <naveenhiranya2005@gmail.com>
2026-06-14 11:18:01 +0530

    Update main.java

--- sr/main.java
+++ sr/main.java
@@ -1,1 +1,13 @@
 
+if (choice == 1) {
+
+    scanner.nextLine(); // consume the leftover newline
+
+    System.out.print("Enter your hero's name: ");
+    String name = scanner.nextLine();
+
+    Player player = new Player(name);
+
+    System.out.println("\nWelcome, " + player.name + "!");
+    player.showStats();
+}
